95787690f85d850169f2f9910e88d693e12c7e66
Taku-Sashima <131934396+Taku-Sashima@users.noreply.github.com>
2025-05-26 21:34:56 +0900

    ディレクトリまで指定可能に変更

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::env;
+use std::io::stdin;
 use std::fs;
-use std::path::Path;
+use std::path::PathBuf;
 
 // テンプレートを埋め込む
 const TEMPLATE_PAGE: &str = include_str!("templates/page.txt");
@@ -9,30 +9,44 @@ const TEMPLATE_SETUP: &str = include_str!("templates/Setup.txt");
 const TEMPLATE_GENERIC: &str = include_str!("templates/template.txt");
 
 fn main() {
-    // コマンドライン引数を取得
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("引数の数が間違っています");
-        std::process::exit(1);
-    }
+    println!("パスカルケースで新しいディレクトリ名を入力してください");
+    println!("パスを指定することもできます");
+
+    // 標準入力でディレクトリ名もしくはパスを受け取る
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+
+    let arg: Vec<&str> = input.trim().split('/').collect();
+    let arg_len: usize = arg.len();
+    let directory_name: &str = arg.last().unwrap_or(&"");
+    let directory_path:PathBuf = arg
+        .iter()
+        .take(arg_len - 1)
+        .fold(PathBuf::new(), |mut acc, &dir| {
+            acc.push(dir);
+            acc
+        });
+
+    println!("ディレクトリ名: {}", directory_name);
+    println!("ディレクトリパス: {}", directory_path.display());
+
 
     // 引数の最初の文字が大文字かどうかをチェック
-    if !args[1].chars().next().unwrap().is_uppercase() {
+    if !directory_name.chars().next().unwrap().is_uppercase() {
         eprintln!("最初の文字は大文字である必要があります");
         std::process::exit(1);
     }
 
     // 引数で受け取った名前をディレクトリ名として使用
-    let dir_name = &args[1];
-    let dir_path = Path::new(dir_name);
+    let dir_path = directory_path.join(directory_name);
 
     // ディレクトリを作成
-    if let Err(e) = fs::create_dir_all(dir_path) {
-        eprintln!("Failed to create directory {}: {}", dir_name, e);
+    if let Err(e) = fs::create_dir_all(&dir_path) {
+        eprintln!("Failed to create directory {}: {}", directory_name, e);
         std::process::exit(1);
     }
 
-    let name = format!("{}.tsx", args[1]); // 引数で受け取った名前に基づくファイル名
+    let name = format!("{}.tsx", directory_name); // 引数で受け取った名前に基づくファイル名
 
     // 作成するファイルのリスト
     let filenames: Vec<(&str, &str)> = vec![
@@ -44,7 +58,7 @@ fn main() {
 
     for (filename, template) in filenames {
         // プレースホルダーを置換
-        let content = template.replace("{placeholder}", args[1].as_str());
+        let content = template.replace("{placeholder}", directory_name);
 
         // ファイルパスを生成
         let file_path = dir_path.join(filename);
