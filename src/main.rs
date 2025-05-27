fb6c9f38def01f1739d75d9499d5b0d5aee21fd3
Taku-Sashima <131934396+Taku-Sashima@users.noreply.github.com>
2025-05-27 15:46:41 +0900

    パスの指定方法と不適なパスを入れた際の処理を変更

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io::stdin;
-use std::fs;
-use std::path::PathBuf;
+use std::fs::{create_dir, create_dir_all, write};
+use std::path::{Path, PathBuf};
 
 // テンプレートを埋め込む
 const TEMPLATE_PAGE: &str = include_str!("templates/page.txt");
@@ -10,43 +10,67 @@ const TEMPLATE_GENERIC: &str = include_str!("templates/template.txt");
 
 fn main() {
     println!("パスカルケースで新しいディレクトリ名を入力してください");
-    println!("パスを指定することもできます (ex: src/app/(pages)/");
+    println!("パスを指定することもできます (ex: src/app/(pages)/ )");
 
     // 標準入力でディレクトリ名もしくはパスを受け取る
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
+    let mut new_dir_name: String;
+    let mut parent_dir:PathBuf;
+    loop{
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
 
-    let arg: Vec<&str> = input.trim().split('/').collect();
-    let arg_len: usize = arg.len();
-    let directory_name: &str = arg.last().unwrap_or(&"");
-    let directory_path:PathBuf = arg
-        .iter()
-        .take(arg_len - 1)
-        .fold(PathBuf::new(), |mut acc, &dir| {
-            acc.push(dir);
-            acc
-        });
+        let arg: Vec<&str> = input.trim().split('/').collect();
+        let arg_len: usize = arg.len();
+        new_dir_name= arg.last().expect("有効なディレクトリ名を入力してください").to_string();
+        parent_dir= arg
+            .iter()
+            .take(arg_len - 1)
+            .fold(PathBuf::new(), |mut acc, &dir| {
+                acc.push(dir);
+                acc
+            });
 
-    println!("ディレクトリ名: {}", directory_name);
-    println!("ディレクトリパス: {}", directory_path.display());
+        println!("ディレクトリ名: {}", new_dir_name);
+        println!("ディレクトリパス: {}", parent_dir.display());
 
-
-    // 引数の最初の文字が大文字かどうかをチェック
-    if !directory_name.chars().next().unwrap().is_uppercase() {
-        eprintln!("最初の文字は大文字である必要があります");
-        std::process::exit(1);
+        // 引数の最初の文字が大文字かどうかをチェック
+        if !new_dir_name.chars().next().unwrap().is_uppercase() {
+            eprintln!("新しく作成するディレクトリ名はパスカルケースで始まる必要があります。");
+        }else{
+            break; // 有効なディレクトリ名が入力された場合はループを抜ける
+        }
     }
-
-    // 引数で受け取った名前をディレクトリ名として使用
-    let dir_path = directory_path.join(directory_name);
+    
 
     // ディレクトリを作成
-    if let Err(e) = fs::create_dir_all(&dir_path) {
-        eprintln!("Failed to create directory {}: {}", directory_name, e);
+    if !Path::exists(&parent_dir) && !parent_dir.as_os_str().is_empty(){
+        println!("新しいディレクトリまでのパス {} が存在しません。新しく作成しますか？ Y/N", parent_dir.display());
+        loop{
+            let mut answer = String::new();
+            stdin().read_line(&mut answer).unwrap();
+            let answer = answer.trim().to_uppercase();
+            if answer == "Y" {
+                if let Err(e) = create_dir_all(&parent_dir) {
+                    eprintln!("ディレクトリの作成に失敗しました: {}", e);
+                    std::process::exit(1);
+                }
+                break;
+            } else if answer == "N" {
+                println!("ディレクトリの作成を中止します。");
+                std::process::exit(0);
+            } else {
+                println!("YまたはNで答えてください。");
+            }
+        }
+    }
+
+    let dir_path = parent_dir.join(&new_dir_name);
+    if let Err(e) = create_dir(&dir_path) {
+        eprintln!("Failed to create directory {}: {}", new_dir_name, e);
         std::process::exit(1);
     }
 
-    let name = format!("{}.tsx", directory_name); // 引数で受け取った名前に基づくファイル名
+    let name = format!("{}.tsx", new_dir_name); // 引数で受け取った名前に基づくファイル名
 
     // 作成するファイルのリスト
     let filenames: Vec<(&str, &str)> = vec![
@@ -58,13 +82,13 @@ fn main() {
 
     for (filename, template) in filenames {
         // プレースホルダーを置換
-        let content = template.replace("{placeholder}", directory_name);
+        let content = template.replace("{placeholder}", &new_dir_name);
 
         // ファイルパスを生成
         let file_path = dir_path.join(filename);
 
         // ファイルを作成
-        if let Err(e) = fs::write(&file_path, content) {
+        if let Err(e) = write(&file_path, content) {
             eprintln!("Failed to create {}: {}", file_path.display(), e);
         } else {
             println!("Created {}", file_path.display());
